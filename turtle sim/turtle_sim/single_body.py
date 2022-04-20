3174bcfcb71c4455e714c0f0b65917f670f92114
shiva-suryaiitm <94967658+shiva-suryaiitm@users.noreply.github.com>
2022-04-20 19:34:58 +0530

    Update single_body.py

--- turtle sim/turtle_sim/single_body.py
+++ turtle sim/turtle_sim/single_body.py
@@ -13,13 +13,13 @@ y_vel = 0
 
 initial_x = float(input("Enter  x position (Which is used as a center): "))
 initial_y = float(input("Enter  y position (Which is used as a cemter): "))
-a = float(input("Enter alpha value (Which is used as F= alpha/d^2) "))
+a = float(input("Enter alpha value (Which is used as F= alpha/d^2) "))            # getting inputs
 initial_vel_x = float(input("Enter Intial x vel : "))
 initial_vel_y = float(input("Enter Intial y vel : "))
 
 def position(data):
     x = round(data.x,3)
-    y = round(data.y,3)
+    y = round(data.y,3)                             # getting position of turtle
     force_vel(x,y,initial_x,initial_y,a)
     
 def get_position():
@@ -46,12 +46,12 @@ def publish_vel():
 def force_vel(x,y,x0,y0,alpha):
     d = ((-x+x0)**2 + (y-y0)**2)**0.5
     F = alpha / (d)**2
-    global x_vel , y_vel
-    vel_x = F*0.005*(x0-x)/d
-    vel_y = F*0.005*(y0-y)/d
-    #print(vel_x , vel_y)
+    global x_vel , y_vel                        # calculates force via the formula F = alpha / d^2
+    vel_x = F*0.005*(x0-x)/d                    # Finds velocity using formula v = at (Here avg velocity is used) v = 1/2*at 
+    vel_y = F*0.005*(y0-y)/d                    # Vx and Vy is find using the x and y comp of force , os trignometry is used here
+    # print(vel_x , vel_y)                       # Vx = Fx * 0.005  ( 0.005 = 1/100 * 1/2 ) frametime = 1/100th second and 1/100*1/2 = 0.005
     x_vel , y_vel = vel_x , vel_y
-    k = time.time()
+    # k = time.time()
     return None
         
         
